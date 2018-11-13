54675f9e4c9236e0ca0932b235a767432afdd1b6
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2018-11-13 22:34:56 +0100

    implement drag events on bottom sheet component

--- projects/bottom-sheet/src/lib/bottom-sheet.component.ts
+++ projects/bottom-sheet/src/lib/bottom-sheet.component.ts
@@ -3,7 +3,14 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'ngx-bottom-sheet',
   template: `
-    <div class="element" #element>
+    <div (mousedown)="onDragStart($event)"
+         (touchstart)="onDragStart($event)"
+         (mousemove)="onDragProgress($event)"
+         (touchmove)="onDragProgress($event)"
+         (mouseup)="onDragEnd($event)"
+         (touchend)="onDragEnd($event)"
+         class="element"
+         #element>
       <div class="handle"></div>
       <div class="content" #content>
         <h4>Hello Ngx Bottom Sheet World!</h4>
@@ -17,13 +24,87 @@ export class BottomSheetComponent implements AfterViewInit {
   @ViewChild('element') element: ElementRef;
   @ViewChild('content') content: ElementRef;
 
+  private active: boolean;
+
+  private initialY = 0;
   private currentY = 0;
+  private offsetY = 0;
+
+  private lastPos = 0;
 
   constructor() { }
 
   ngAfterViewInit(): void {
     this.currentY = window.innerHeight - 40;
+    this.offsetY = this.currentY;
     this.move();
+  }
+
+  onDragStart(event: MouseEvent | TouchEvent): void {
+    this.active = true;
+
+    if (event instanceof MouseEvent) {
+      this.initialY = event.clientY - this.offsetY;
+      this.lastPos = event.clientY;
+    }
+    if (event instanceof TouchEvent) {
+      this.initialY = event.touches[0].clientY - this.offsetY;
+      this.lastPos = event.touches[0].clientY;
+    }
+  }
+
+  onDragProgress(event: MouseEvent | TouchEvent): void {
+    if (this.active) {
+      event.preventDefault();
+
+      if (event instanceof MouseEvent) {
+        this.currentY = event.clientY - this.initialY;
+      }
+      if (event instanceof TouchEvent) {
+        this.currentY = event.touches[0].clientY - this.initialY;
+      }
+
+      this.offsetY = this.currentY;
+
+      this.move();
+    }
+  }
+
+  onDragEnd(event: MouseEvent | TouchEvent): void {
+    this.active = false;
+
+    let pos: number;
+    if (event instanceof MouseEvent) {
+      if (this.lastPos < event.clientY) {
+        // bottom position
+        pos = window.innerHeight - 40;
+      } else {
+        // top position
+        pos = window.innerHeight - this.content.nativeElement.clientHeight - 60;
+      }
+    }
+    if (event instanceof TouchEvent) {
+      if (this.lastPos < event.changedTouches[0].clientY) {
+        // bottom position
+        pos = window.innerHeight - 40;
+      } else {
+        // top position
+        pos = window.innerHeight - this.content.nativeElement.clientHeight - 60;
+      }
+    }
+
+    this.currentY = pos;
+    this.offsetY = this.currentY;
+    this.move();
+
+    this.initialY = this.currentY;
+
+    if (event instanceof MouseEvent) {
+      this.lastPos = event.clientY;
+    }
+    if (event instanceof TouchEvent) {
+      this.lastPos = event.changedTouches[0].clientY;
+    }
   }
 
   private move(): void {
