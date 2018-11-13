4bb74ec8b66a7da93b5452c7ef2fe38b3f4bfa78
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2018-11-13 22:12:00 +0100

    bottom sheet component integration

--- projects/bottom-sheet/src/lib/bottom-sheet.component.ts
+++ projects/bottom-sheet/src/lib/bottom-sheet.component.ts
@@ -3,10 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngx-bottom-sheet',
   template: `
-    <p>
-      bottom-sheet works!
-    </p>
+    <div class="element">
+      <div class="handle"></div>
+      <div class="content">
+        <h4>Hello Ngx Bottom Sheet World!</h4>
+      </div>
+    </div>
   `,
-  styles: []
+  styleUrls: ['./bottom-sheet.component.scss']
 })
 export class BottomSheetComponent { }
