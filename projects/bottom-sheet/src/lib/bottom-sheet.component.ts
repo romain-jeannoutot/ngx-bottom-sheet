5a64a6fbf9eac7a56f3c1698e331bd7310ed99f1
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2018-11-13 22:25:19 +0100

    get element and content after view init on bottom sheet component

--- projects/bottom-sheet/src/lib/bottom-sheet.component.ts
+++ projects/bottom-sheet/src/lib/bottom-sheet.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'ngx-bottom-sheet',
   template: `
-    <div class="element">
+    <div class="element" #element>
       <div class="handle"></div>
-      <div class="content">
+      <div class="content" #content>
         <h4>Hello Ngx Bottom Sheet World!</h4>
       </div>
     </div>
   `,
   styleUrls: ['./bottom-sheet.component.scss']
 })
-export class BottomSheetComponent { }
+export class BottomSheetComponent implements AfterViewInit {
+
+  @ViewChild('element') element: ElementRef;
+  @ViewChild('content') content: ElementRef;
+
+  constructor() { }
+
+  ngAfterViewInit(): void { }
+
+}
